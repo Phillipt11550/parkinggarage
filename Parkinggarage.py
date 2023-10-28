e8b80db4b2621b068198ec09541706ece9299805
Phillip Thompson <phillipt11550@gmail.com>
2023-10-28 08:06:33 -0400

    UPDATE

--- Parkinggarage.py
+++ Parkinggarage.py
@@ -1,26 +1,47 @@
 class ParkingGarage:
     def __init__(self, tickets, parking_spaces):
-        self.tickets = tickets
+        self.tickets = list(range(1, tickets + 1))
         self.parking_spaces = parking_spaces
         self.current_ticket = {}
 
     def takeTicket(self):
-        self.tickets -= 1
-        self.parking_spaces -= 1
+        if self.tickets:
+            ticket = self.tickets.pop(0)
+            self.current_ticket[ticket] = False
+            print(f"Ticket number {ticket} has been issued.")
+        else:
+            print("No more tickets available.")
 
-    def payForParking(self):
+    def payForParking(self, ticket):
         payment = input("Please enter your payment: ")
         if payment:
             print("Your ticket has been paid and you have 15 mins to leave.")
-            self.current_ticket["paid"] = True
+            self.current_ticket[ticket] = True
 
-    def leaveGarage(self):
-        if self.current_ticket.get("paid"):
+    def leaveGarage(self, ticket):
+        if self.current_ticket.get(ticket):
             print("Thank You, have a nice day.")
+            del self.current_ticket[ticket]
+            self.tickets.append(ticket)
+            self.tickets.sort()
         else:
-            self.payForParking()
-            print("Thank You, have a nice day!")
-        self.parking_spaces += 1
-        self.tickets += 1
-        
-garage = ParkingGarage(10, 10)
+            print("Please pay for your parking before leaving.")
+
+def run_parking_garage():
+    garage = ParkingGarage(10, 10)
+    while True:
+        action = input("What would you like to do? (take/pay/leave/quit): ")
+        if action.lower() == "take":
+            garage.takeTicket()
+        elif action.lower() == "pay":
+            ticket = int(input("Please enter your ticket number: "))
+            garage.payForParking(ticket)
+        elif action.lower() == "leave":
+            ticket = int(input("Please enter your ticket number: "))
+            garage.leaveGarage(ticket)
+        elif action.lower() == "quit":
+            break
+        else:
+            print("Invalid action. Please try again.")
+
+run_parking_garage()
